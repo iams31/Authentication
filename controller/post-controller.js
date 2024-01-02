b1207cf5b075e090d06955e3f953167b0c862248
Sameer Kumar <sameerkumarroy073@gmail.com>
2024-01-02 18:27:22 +0530

    Comment is added to the home page,and delete action is aded to the project

--- controller/post-controller.js
+++ controller/post-controller.js
@@ -1,4 +1,5 @@
 const Post = require("../models/posts");
+const Comment = require("../models/comment");
 module.exports.create = (req, res) => {
   Post.create({
     content: req.body.content,
@@ -8,5 +9,25 @@ module.exports.create = (req, res) => {
     .catch((err) => {
       console.log("Error while creating a user");
       res.redirect("back");
+    });
+};
+
+module.exports.destroy = (req, res) => {
+  Post.findById(req.params.id)
+    .then((post) => {
+      if (post) {
+        if (post.user == req.user.id) {
+          //.id is used to convert the object to string this is default functionality that mongoose provide us
+          post.remove();
+          Comment.deleteMany({ post: req.params.id }).catch((err) => {
+            res.redirect("back");
+          });
+        }
+      } else {
+        res.redirect("back");
+      }
+    })
+    .catch((err) => {
+      console.log("Error while finding the post in deleting the post");
     });
 };
