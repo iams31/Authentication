d45fd2af461c48b9b5c8b568933bf5da6df7b059
Sameer Kumar <sameerkumarroy073@gmail.com>
2024-01-03 15:49:06 +0530

    Profile modified and also the destroy comment controller is added

--- controller/comments_controller.js
+++ controller/comments_controller.js
@@ -27,3 +27,31 @@ module.exports.create = (req, res) => {
       res.redirect("back");
     });
 };
+module.exports.destroy = (req, res) => {
+  Comment.findById(req.params.id)
+    .then((comment) => {
+      if (comment.user == req.user.id) {
+        let postId = comment.post;
+        comment
+          .deleteOne()
+          .then(() => {
+            return Post.findByIdAndUpdate(postId, {
+              $pull: { comments: req.params.id },
+            });
+          })
+          .then(() => {
+            res.redirect("back");
+          })
+          .catch((err) => {
+            res
+              .status(500)
+              .send("Error deleting comment and updating post: " + err.message);
+          });
+      } else {
+        res.redirect("/users/sign-in");
+      }
+    })
+    .catch(() => {
+      res.redirect(500).send("Error while sending comment");
+    });
+};
